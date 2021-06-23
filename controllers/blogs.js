721667823729557b743d75c114d44ce6a273001e
Shinya YAKU <syaku@yahoo-corp.jp>
2021-06-24 00:36:38 +0900

    part 4d

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const { requestLogger } = require('../utils/middleware')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -8,7 +18,25 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog(
+    {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: 2,
+      user: user.id
+    }
+  )
+
   const result = await blog.save()
   response.status(201).json(result)
 })
